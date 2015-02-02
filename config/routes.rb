0077ff9371f6f45fd60b3b62925e05bb7bcea87e
tengr <tengrc1106@gmail.com>
2015-02-02 12:26:54 +1100

    Update Gemfile.lock for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
+  get 'convert', to: 'convert#index'
+  get 'convert/:flat/:flatdec/:flon/:flondec/:tlat/:tolatdec/:tlon/:tlondec', to: 'convert#trans'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
